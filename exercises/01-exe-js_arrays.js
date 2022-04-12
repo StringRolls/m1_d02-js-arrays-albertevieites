3fa79bfa25b9228574db925035fdeb959b0e0f32
Alberte Vieites <albertevieites@tutanota.com>
2022-04-12 12:24:21 +0200

    add: 01 exercise completed

--- exercises/01-exe-js_arrays.js
+++ exercises/01-exe-js_arrays.js
@@ -1,31 +1,40 @@
 // Practice JS Arrays
 
 //Given the array
-const favorites = ['javascript', 'html', 'css'];
+const favorites = ["javascript", "html", "css"];
 
 // iterate over it and console.log each element
 // ... your code here
-
+for (const element of favorites) {
+    console.log(element);
+}
 // remove first element
+favorites.shift();
 // ... your code here
 console.log(favorites); // => [ 'html', 'css' ]
 
 // remove last element
+favorites.pop();
 // ... your code here
 console.log(favorites); // => [ 'html' ]
 
 // add 'react' on the first place
+favorites.unshift('react');
 // ... your code here
 console.log(favorites); // => [ 'react', 'html' ]
 
 // add 'node' to the last place
+favorites.push('node');
 // ... your code here
 console.log(favorites); // => [ 'react', 'html', 'node' ]
 
 // remove element in the position 2 and add 'express'
+favorites.splice(2, 1, 'express');
 // ... your code here
 console.log(favorites); // => [ 'react', 'express', 'node' ]
 
 // remove elements in the positions 2 and 3 and add 'mongodb'
 // ... your code here
-console.log(favorites); // => [ 'react', 'mongodb' ]
+favorites.unshift('javascript');
+favorites.splice(2, 2, 'mongodb')
+console.log(favorites); // => [ 'javascript' 'react', 'mongodb' ]
